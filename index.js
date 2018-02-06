cc902f749c5863f64631d42099bf551eb519a35a
Carine Tomassian <carinetomassian@gmail.com>
2018-02-06 16:24:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(katzDeliLine) {
     var theLine = [] ;
     let i = 0;
     for (var i = 0; i < katzDeliLine.length; i++) {
-    theLine.push([i+1] + ". " + katzDeliLine[i]);
+    theLine.push([i+1] + ". " + katzDeliLine[i]) + " ";
     }
     return "The line is currently: " + theLine;
   }
